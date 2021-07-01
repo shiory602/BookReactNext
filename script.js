1595bc477ca8a5e0bfa25b9630b45720e53133c4
shiory602 <suzushioufo@gmail.com>
2021-07-01 16:49:38 -0700

    design with bootstrap

--- script.js
+++ script.js
@@ -9,19 +9,22 @@ let element = React.createElement(
         React.createElement(
             'h2', {}, "Hello!"
         ),
+        // React.createElement(
+        //     'h3', {}, "React sample page."
+        // ),
         React.createElement(
-            'h3', {}, "React sample page."
+            'h4', {className:'alert alert-primary'}, "React sample page."
         ),
         React.createElement(
-            'ul', {}, [
+            'ul', {className:'list-group'}, [
                 React.createElement(
-                    'li', {}, "First item."
+                    'li', {className: 'list-group-item'}, "First item."
                 ),
                 React.createElement(
-                    'li', {}, "Second item."
+                    'li', {className: 'list-group-item'}, "Second item."
                 ),
                 React.createElement(
-                    'li', {}, "Third item."
+                    'li', {className: 'list-group-item'}, "Third item."
                 ),
             ]
         ),
