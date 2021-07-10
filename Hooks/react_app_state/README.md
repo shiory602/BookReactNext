a5e3af1730be5c32e4228205c8d16a67f2fa688a
shiory602 <suzushioufo@gmail.com>
2021-07-10 13:09:15 -0700

    click state

--- Hooks/react_app_state/README.md
+++ Hooks/react_app_state/README.md
@@ -16,4 +16,112 @@ JavaScriptでは、複数の値を返す関数を作成できる。
 これを**分割代入**という。
 ここでは、戻り値を代入する変数を配列にしたものが使われる。
 
-## ステートを表示する
+# ステートを表示する
+1. ステートを作成
+```js
+const [message] = useState("Welcome to Hooks!")
+```
+2. JSX内に埋め込んで表示
+```js
+<p className="h5">{message}</p>
+```
+コード全体
+```js
+import React, { useState } from 'react'
+import './App.css';
+
+function App() {
+  const [message] = useState("Welcome to Hooks!")
+
+  return (
+    <div className="App">
+      <h1 className="bg-primary text-white display-4 ">React</h1>
+      <div className="container">
+        <h4 className="my-3">Hooks sample</h4>
+        <div className="alert alert-primary text-center">
+          <p className="h5">{message}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+```
+
+# ステートで数字をカウント
+1. ステートを用意
+```js
+const [count, setCount] = useState(0)
+```
+2. ステートを変更する関数を用意
+```js
+const clickFunc = () => {
+    setCount(count + 1)
+}
+```
+3. ステートを表示
+```js
+<p className="h5 mb-3">click: {count} times!</p>
+```
+4. クリックで値の変更を行う処理を用意
+```js
+<div><button className="btn btn-primary" onClick={clickFunc}>
+    Click me
+</button></div>
+```
+### 全体
+```js
+// list 4-1
+// import React, { useState } from 'react'
+// import './App.css';
+
+// function App() {
+//   const [message] = useState("Welcome to Hooks!")
+
+//   return (
+//     <div className="App">
+//       <h1 className="bg-primary text-white display-4 ">React</h1>
+//       <div className="container">
+//         <h4 className="my-3">Hooks sample</h4>
+//         <div className="alert alert-primary text-center">
+//           <p className="h5">{message}</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// list 4-2
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const clickFunc = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
+      <h1 className="bg-primary text-white display-4 ">React</h1>
+      <div className="container">
+        <h4 className="my-3">Hooks sample</h4>
+        <div className="alert alert-primary text-center">
+          <p className="h5 mb-3">click: {count} times!</p>
+          <div><button className="btn btn-primary"
+                  onClick={clickFunc}>
+                    Click me
+              </button></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+```
--- Hooks/react_app_state/src/App.js
+++ Hooks/react_app_state/src/App.js
@@ -1,16 +1,99 @@
+// list 4-1
+// import React, { useState } from 'react'
+// import './App.css';
+
+// function App() {
+//   const [message] = useState("Welcome to Hooks!")
+
+//   return (
+//     <div className="App">
+//       <h1 className="bg-primary text-white display-4 ">React</h1>
+//       <div className="container">
+//         <h4 className="my-3">Hooks sample</h4>
+//         <div className="alert alert-primary text-center">
+//           <p className="h5">{message}</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// // list 4-2
+// import React, { useState } from 'react'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+//   const clickFunc = () => {
+//     setCount(count + 1)
+//   }
+
+//   return (
+//     <div>
+//       <h1 className="bg-primary text-white display-4 ">React</h1>
+//       <div className="container">
+//         <h4 className="my-3">Hooks sample</h4>
+//         <div className="alert alert-primary text-center">
+//           <p className="h5 mb-3">click: {count} times!</p>
+//           <div><button className="btn btn-primary"
+//                   onClick={clickFunc}>
+//                     Click me
+//               </button></div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// list 4-3
 import React, { useState } from 'react'
-import './App.css';
+import './App.css'
 
 function App() {
-  const [message] = useState("Welcome to Hooks!")
+  const [count, setCount] = useState(0)
+  const [flag, setFlag] = useState(false)
+  const clickFunc = () => {
+    setCount(count + 1)
+  }
+  const changeFlag = (e) => {
+    setFlag(e.target.checked)
+  }
 
   return (
-    <div className="App">
+    <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
+        {flag ?
         <div className="alert alert-primary text-center">
-          <p className="h5">{message}</p>
+          <p className="h5 mb-3">click: {count} times!</p>
+          <div><button className="btn btn-primary"
+                  onClick={clickFunc}>
+                    Click me
+              </button></div>
+        </div>
+        :
+        <div className="alert alert-primary text-center">
+          <p className="h5 mb-3 text-left text-primary">
+            click: {count} times!</p>
+          <div><button className="btn btn-primary"
+                  onClick={clickFunc}>
+                    Click me
+              </button></div>
+        </div>
+        }
+        <div className="form-group h6 text-center pt-3">
+          <input type="checkbox" className="form-check-input"
+            id="check1" onChange={changeFlag} />
+          <label className="form-check-label" htmlFor="check1">
+            Change form style.
+          </label>
         </div>
       </div>
     </div>
