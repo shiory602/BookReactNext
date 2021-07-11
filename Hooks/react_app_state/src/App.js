09f8014627aa7283aa5e0af6086ee8f46269718c
shiory602 <suzushioufo@gmail.com>
2021-07-10 17:54:47 -0700

    useState in child component

--- Hooks/react_app_state/src/App.js
+++ Hooks/react_app_state/src/App.js
@@ -170,41 +170,100 @@
 // export default App;
 
 
-// list 4-6
+// // list 4-6
+// import React, { useState } from 'react'
+// import './App.css'
+
+// function AlertMessage(props) {
+//   return <div className="alert alert-primary h5 text-primary">
+//     {props.message}
+//   </div>
+// }
+
+// function CardMessage(props) {
+//   return <div className="card p-3 h5 border-primary text-center">
+//     {props.message}
+//   </div>
+// }
+
+// function App() {
+//   const [msg, setMsg] = useState("This is sample message!")
+
+//   const doAction = () => {
+//     let res = window.prompt('type your name:')
+//     setMsg("Hello, " + res + "!!")
+//   }
+
+//   return (
+//     <div>
+//       <h1 className="bg-primary text-white display-4 ">React</h1>
+//       <div className="container">
+//         <h4 className="my-3">Hooks sample</h4>
+//         <AlertMessage message={msg} />
+//         <CardMessage message={msg} />
+//         <div className="text-center">
+//           <button onClick={doAction} className="btn btn-primary">
+//             Click me!
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// list 4-7
 import React, { useState } from 'react'
 import './App.css'
 
 function AlertMessage(props) {
+  const data = ["Hello!", "Welcome...", "Good-bye?"]
+
+  const actionAlert = () => {
+    const re = data[Math.floor(Math.random() * data.length)]
+    props.setAlert('message: "' + re + '".')
+  }
+
   return <div className="alert alert-primary h5 text-primary">
-    {props.message}
+    <h5>{props.alert}</h5>
+    <button onClick={actionAlert} className="btn btn-primary">
+      Click me!
+    </button>
   </div>
 }
 
 function CardMessage(props) {
+  const [count, setCount] = useState(0)
+
+  const actionCard = () => {
+    setCount(count + 1)
+    props.setCard("card counter: " + count + " count.")
+  }
+
   return <div className="card p-3 h5 border-primary text-center">
-    {props.message}
+    <h5>{props.card}</h5>
+    <button onClick={actionCard} className="btn btn-secondary">
+      Click me!
+    </button>
   </div>
 }
 
 function App() {
-  const [msg, setMsg] = useState("This is sample message!")
-
-  const doAction = () => {
-    let res = window.prompt('type your name:')
-    setMsg("Hello, " + res + "!!")
-  }
+  const [alert, setAlert] = useState("This is alert message!")
+  const [card, setCard] = useState("This is card message!")
 
   return (
     <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        <AlertMessage message={msg} />
-        <CardMessage message={msg} />
-        <div className="text-center">
-          <button onClick={doAction} className="btn btn-primary">
-            Click me!
-          </button>
+        <AlertMessage alert={alert} setAlert={setAlert} />
+        <CardMessage card={card} setCard={setCard} />
+        <div className="text-right">
+          <p>{alert}</p>
+          <p>{card}</p>
         </div>
       </div>
     </div>
