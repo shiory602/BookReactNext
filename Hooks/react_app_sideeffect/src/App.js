5c409aa7e55d13ec3b5c2a2eea176ee8848f79f5
shiory602 <suzushioufo@gmail.com>
2021-07-11 11:58:00 -0700

    side effect

--- Hooks/react_app_sideeffect/src/App.js
+++ Hooks/react_app_sideeffect/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
+function AlertMessage(props) {
+  return <div className="alert alert-primary h5 text-primary">
+    <h5>{props.msg}</h5>
+  </div>
+}
+
 function App() {
+  const [val, setVal] = useState(0)
+  const [msg, setMsg] = useState('set a number...')
+
+  const doChange = (event) => {
+    setVal(event.target.value)
+  }
+
+  useEffect(() => {
+    let total = 0
+    for (let i = 0; i <= val; i++) {
+      total += i
+    }
+    setMsg("total: " + total + ".")
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="bg-primary text-white display-4 ">React</h1>
+      <div className="container">
+        <h4 className="my-3">Hooks sample</h4>
+        <AlertMessage msg={msg} />
+        <div className="form-group">
+          <label>Input:</label>
+          <input type="number" className="form-control"
+            onChange={doChange} />
+        </div>
+      </div>
     </div>
   );
 }
