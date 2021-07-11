9968504701731212386d8c3a27049b29477a1f9d
shiory602 <suzushioufo@gmail.com>
2021-07-11 12:06:16 -0700

    multiple useEffect

--- Hooks/react_app_sideeffect/src/App.js
+++ Hooks/react_app_sideeffect/src/App.js
@@ -1,3 +1,49 @@
+// // 4-9
+// import React, { useState, useEffect } from 'react'
+// import './App.css';
+
+// function AlertMessage(props) {
+//   return <div className="alert alert-primary h5 text-primary">
+//     <h5>{props.msg}</h5>
+//   </div>
+// }
+
+// function App() {
+//   const [val, setVal] = useState(0)
+//   const [msg, setMsg] = useState('set a number...')
+
+//   const doChange = (event) => {
+//     setVal(event.target.value)
+//   }
+
+//   useEffect(() => {
+//     let total = 0
+//     for (let i = 0; i <= val; i++) {
+//       total += i
+//     }
+//     setMsg("total: " + total + ".")
+//   })
+
+//   return (
+//     <div>
+//       <h1 className="bg-primary text-white display-4 ">React</h1>
+//       <div className="container">
+//         <h4 className="my-3">Hooks sample</h4>
+//         <AlertMessage msg={msg} />
+//         <div className="form-group">
+//           <label>Input:</label>
+//           <input type="number" className="form-control"
+//             onChange={doChange} />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// 4-10
 import React, { useState, useEffect } from 'react'
 import './App.css';
 
@@ -8,19 +54,29 @@ function AlertMessage(props) {
 }
 
 function App() {
-  const [val, setVal] = useState(0)
-  const [msg, setMsg] = useState('set a number...')
+  const [val, setVal] = useState(1000)
+  const [tax1, setTax1] = useState(0)
+  const [tax2, setTax2] = useState(0)
+  const [msg, setMsg] = useState(<p>set a price...</p>)
 
   const doChange = (event) => {
     setVal(event.target.value)
   }
 
+  const doAction = () => {
+    let res = <div>
+      <p>軽減税率（8%）：{tax1} 円</p>
+      <p>通常税率（10%）：{tax2} 円</p>
+    </div>
+    setMsg(res)
+  }
+
   useEffect(() => {
-    let total = 0
-    for (let i = 0; i <= val; i++) {
-      total += i
-    }
-    setMsg("total: " + total + ".")
+    setTax1(Math.floor(val * 1.08))
+  })
+
+  useEffect(() => {
+    setTax2(Math.floor(val * 1.1))
   })
 
   return (
@@ -34,6 +90,8 @@ function App() {
           <input type="number" className="form-control"
             onChange={doChange} />
         </div>
+        <button className="btn btn-primary"
+          onClick={doAction}>Calc</button>
       </div>
     </div>
   );
