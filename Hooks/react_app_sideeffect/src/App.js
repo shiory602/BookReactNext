1df3188ddc521a7a22db7ae1b0796fca8a4deb10
shiory602 <suzushioufo@gmail.com>
2021-07-11 12:22:01 -0700

    second args

--- Hooks/react_app_sideeffect/src/App.js
+++ Hooks/react_app_sideeffect/src/App.js
@@ -43,7 +43,66 @@
 // export default App;
 
 
-// 4-10
+// // 4-10
+// import React, { useState, useEffect } from 'react'
+// import './App.css';
+
+// function AlertMessage(props) {
+//   return <div className="alert alert-primary h5 text-primary">
+//     <h5>{props.msg}</h5>
+//   </div>
+// }
+
+// function App() {
+//   const [val, setVal] = useState(1000)
+//   const [tax1, setTax1] = useState(0)
+//   const [tax2, setTax2] = useState(0)
+//   const [msg, setMsg] = useState(<p>set a price...</p>)
+
+//   // 入力値の更新だけ
+//   const doChange = (event) => {
+//     setVal(event.target.value)
+//   }
+
+//   // 結果の表示だけ
+//   const doAction = () => {
+//     let res = <div>
+//       <p>軽減税率（8%）：{tax1} 円</p>
+//       <p>通常税率（10%）：{tax2} 円</p>
+//     </div>
+//     setMsg(res)
+//   }
+
+//   useEffect(() => {
+//     setTax1(Math.floor(val * 1.08))
+//   })
+
+//   useEffect(() => {
+//     setTax2(Math.floor(val * 1.1))
+//   })
+
+//   return (
+//     <div>
+//       <h1 className="bg-primary text-white display-4 ">React</h1>
+//       <div className="container">
+//         <h4 className="my-3">Hooks sample</h4>
+//         <AlertMessage msg={msg} />
+//         <div className="form-group">
+//           <label>Input:</label>
+//           <input type="number" className="form-control"
+//             onChange={doChange} />
+//         </div>
+//         <button className="btn btn-primary"
+//           onClick={doAction}>Calc</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// 4-11
 import React, { useState, useEffect } from 'react'
 import './App.css';
 
@@ -64,14 +123,14 @@ function App() {
     setVal(event.target.value)
   }
 
-  // 結果の表示だけ
-  const doAction = () => {
+  // ★新たに追加したフック
+  useEffect(() => {
     let res = <div>
       <p>軽減税率（8%）：{tax1} 円</p>
       <p>通常税率（10%）：{tax2} 円</p>
     </div>
     setMsg(res)
-  }
+  }, [tax1, tax2])
 
   useEffect(() => {
     setTax1(Math.floor(val * 1.08))
@@ -92,8 +151,6 @@ function App() {
           <input type="number" className="form-control"
             onChange={doChange} />
         </div>
-        <button className="btn btn-primary"
-          onClick={doAction}>Calc</button>
       </div>
     </div>
   );
