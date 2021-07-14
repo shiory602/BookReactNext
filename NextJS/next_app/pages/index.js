5564541c091100cb337ff4124d8304aa4de9e37f
shiory602 <suzushioufo@gmail.com>
2021-07-14 12:40:03 -0700

    header

--- NextJS/next_app/pages/index.js
+++ NextJS/next_app/pages/index.js
@@ -72,9 +72,55 @@
 // }
 
 
-// list 5-4
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// // list 5-4
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+
+// export default function Home() {
+//   let title = "Next.js page"
+//   let message = "React Next.js sample page."
+
+//   return (
+//     <div>
+//       <Head>
+//         <title>{title}</title>
+//         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossOrigin="anonymous" />
+//       </Head>
+//       <style jsx>{`
+//         h1 {
+//           textAlign: center;
+//         }
+
+//         h2 {
+//           textAlign: center;
+//           margin: 0px 5px;
+//           color: #aad;
+//           font-size: 36pt;
+//           font-weight: bold;
+//         }
+
+//         p {
+//           textAlign: center;
+//           margin: 0px 5px;
+//           color: blue;
+//           fontSize: 18pt;
+//         }
+//       `}</style>
+
+//       <h1 className="bg-primary text-white display-4 ">React</h1>
+//       <div className="container">
+//         <h2 className="my-3">{title}</h2>
+//         <div className="alert alert-primary text-center">
+//           <p className="h5">{message}.</p>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+
+// list 5-6
+import Header from './header'
 
 export default function Home() {
   let title = "Next.js page"
@@ -82,35 +128,11 @@ export default function Home() {
 
   return (
     <div>
-      <Head>
-        <title>{title}</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossOrigin="anonymous" />
-      </Head>
-      <style jsx>{`
-        h1 {
-          textAlign: center;
-        }
-
-        h2 {
-          textAlign: center;
-          margin: 0px 5px;
-          color: #aad;
-          font-size: 36pt;
-          font-weight: bold;
-        }
-
-        p {
-          textAlign: center;
-          margin: 0px 5px;
-          color: blue;
-          fontSize: 18pt;
-        }
-      `}</style>
-
-      <h1 className="bg-primary text-white display-4 ">React</h1>
+      <Header title={title} />
+      <h1 className="bg-primary text-white display-4 text-right">React</h1>
       <div className="container">
-        <h2 className="my-3">{title}</h2>
-        <div className="alert alert-primary text-center">
+        <h3 className="my-3 text-primary text-center">{title}</h3>
+        <div className="alert alert-primary text-left">
           <p className="h5">{message}.</p>
         </div>
       </div>
