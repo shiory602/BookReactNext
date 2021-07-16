408a9c94326ec659bb0983edb8d2f2dbf245dc06
shiory602 <suzushioufo@gmail.com>
2021-07-16 12:11:00 -0700

    layout

---
+++ NextJS/next_app_routing/pages/other.js
@@ -0,0 +1,23 @@
+import Header from './header'
+import Link from 'next/link'
+
+export default function Other() {
+    const title = "Other"
+
+    return (
+        <div>
+            <Header title={title} />
+            <h1 className="bg-primary px-3 text-white display-4">
+                React
+            </h1>
+            <div className="card p-3">
+                <p>
+                    これは、もう一つのページの表示です。
+                </p>
+                <Link href="/">
+                    <a>&lt;&lt; Back to Index page</a>
+                </Link>
+            </div>
+        </div>
+    )
+}
